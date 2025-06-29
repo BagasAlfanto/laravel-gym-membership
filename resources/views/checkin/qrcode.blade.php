7383bc41cd1354c8e589d1ad43e194b4b90fd9b1
Regita Hutri Cahyaningrum <541211147@student.smktelkom-pwt.sch.id>
2025-06-29 21:21:19 +0700

    qrcode checkoin

---
+++ resources/views/checkin/qrcode.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  @vite('resources/css/app.css')
+
+  <title>Qr Code</title>
+</head>
+<body class="bg-gray-100 min-h-screen flex items-center justify-center">
+  <div class="mx-auto w-full max-w-sm rounded-xl bg-white p-6 shadow-lg outline outline-black/5">
+    <div class="w-full">
+      <img src="https://i.pinimg.com/736x/49/27/bd/4927bd8ba9f3c281ba366eded4d11fd8.jpg"
+           alt="Profile Image"
+           class="w-50 mx-auto mb-4 shadow-md" />
+      
+      <h1 class="text-center font-bold text-lg">Bagas Alfanto</h1>
+      <p class="text-center text-sm mb-4">Email: bagis@gmail.com</p>
+      
+      <div class="text-left text-gray-500 text-sm">
+        <p>Membership: 10 Jan 2025 to 10 Jan 2026</p>
+        <p>Package: Member Corporate</p>
+        <p>(17 Jun to 17 Jul 2025)</p>
+        <p>Quota Private Trainer: 0</p>
+      </div>
+      
+      <div class="mt-6 text-center">
+        <button class="text-sm text-blue-600 hover:underline focus:outline-none">
+          Profile
+        </button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
