48675e2db2e8f5c75613e201146643f1488d7fcc
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-06-30 21:51:53 +0700

    chore(migrations): update user table field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,15 +14,20 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('no_hp')->unique();
+            $table->timestamp('no_hp_verified_at')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('alamat')->nullable();
+            $table->date('tanggal_lahir')->nullable();
             $table->string('password');
+            $table->string('role')->default('member');
+            $table->string('status')->default('active');
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('no_hp')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
