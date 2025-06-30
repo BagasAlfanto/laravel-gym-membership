0d563bc2efaa84e373d0b6f53b897c4330e5f8d1
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-06-30 21:54:53 +0700

    feat(routes): update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
+    return view('landing.index');
+});
+
+// Authentication routes
+Route::resource('/login', LoginController::class)->only(['index', 'store']);
+Route::resource('/register', RegisterController::class)->only(['index', 'store']);
+
+// Check-in routes
+Route::get('/qrcode', function () {
     return view('checkin.qrcode');
 });
