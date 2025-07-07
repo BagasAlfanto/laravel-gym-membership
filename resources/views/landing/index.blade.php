dd54348704193aca9235778bb37eaa22277e7d7b
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-07-08 01:11:46 +0700

    chore(views/landing): test landing page

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -10,7 +10,7 @@
 <body>
     <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <h1>Ini landing Page Sementara</h1>
-        <button class="bg-blue-500 text-white px-4 py-2 rounded"><a href="{{ route('login.index') }}">Login</a></button>
+        <button class="bg-blue-500 text-white px-4 py-2 rounded"><a href="{{ route('login') }}">Login</a></button>
         <button class="bg-green-500 text-white px-4 py-2 rounded"><a href="{{ route('register.index') }}">Register</a></button>
     </div>
 </body>
