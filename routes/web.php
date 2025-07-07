0437c471ec770f9e8ac596a947d4e2155073e6be
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-07-08 01:12:15 +0700

    chore(routes): update route and add middleware

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Middleware\RoleMiddleware;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
     return view('landing.index');
 });
 
-// Authentication routes
-Route::resource('/login', LoginController::class)->only(['index', 'store']);
-Route::resource('/register', RegisterController::class)->only(['index', 'store']);
+Route::middleware('guest')->group(function (){
+    // Authentication routes
+    Route::resource('/register', RegisterController::class)->only(['index', 'store']);
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'store'])->name('login.store');
+});
 
-Route::post('/logout', [LoginController::class, 'destroy'])
-      ->middleware('auth')
-      ->name('logout');
+Route::middleware(['auth'])->group(function () {
+    // Dashboard
+    Route::get('/dashboard', function () {
+        return view('dashboard.index');
+    })->name('admin.dashboard')
+    ->middleware([RoleMiddleware::class, 'role:admin']);
 
+    // Checkin
+    Route::get('/checkin', function () {
+        return view('checkin.qrcode');
+    })->name('checkin.index')
+    ->middleware([RoleMiddleware::class, 'role:member']);
 
-// Check-in routes
-Route::get('/qrcode', function () {
-    return view('checkin.qrcode');
+    // Logout route
+    Route::get('/logout', [LoginController::class, 'destroy'])->name('logout');
 });
