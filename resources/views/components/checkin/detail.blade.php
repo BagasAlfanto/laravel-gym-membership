259cd942c7ced5e47d5271b8805542c2cb613596
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-07-09 14:59:45 +0700

    feat(component): make checkin component

---
+++ resources/views/components/checkin/detail.blade.php
@@ -0,0 +1,21 @@
+<h1 class="text-center font-bold text-xl mb-1">Bagas Alfanto</h1>
+<p class="text-center text-gray-600 text-sm mb-4">No HP: 123456789</p>
+
+<div class="w-full bg-gray-50 rounded-lg p-4 mb-4 shadow-inner">
+    <div class="flex justify-between text-sm mb-1">
+        <span class="font-medium text-gray-700">Membership</span>
+        <span class="text-gray-600">10 Jan 2025 - 10 Jan 2026</span>
+    </div>
+    <div class="flex justify-between text-sm mb-1">
+        <span class="font-medium text-gray-700">Package</span>
+        <span class="text-gray-600">Member Corporate</span>
+    </div>
+    <div class="flex justify-between text-sm mb-1">
+        <span class="font-medium text-gray-700">Active Period</span>
+        <span class="text-gray-600">(17 Jun - 17 Jul 2025)</span>
+    </div>
+    <div class="flex justify-between text-sm">
+        <span class="font-medium text-gray-700">Quota Private Trainer</span>
+        <span class="text-gray-600">0</span>
+    </div>
+</div>
---
+++ resources/views/components/checkin/qrcode.blade.php
@@ -0,0 +1,12 @@
+<div class="card-container mb-4">
+    <div class="card">
+        <div class="front">
+            <img src="https://i.pinimg.com/736x/49/27/bd/4927bd8ba9f3c281ba366eded4d11fd8.jpg"
+                alt="Profile Image" class="w-42 h-42 mx-auto mb-4 shadow-md border-4 border-gray-200" />
+        </div>
+        <div class="back">
+            <img src="https://i.pinimg.com/736x/90/98/31/90983103e9f414d15596a6b7bf6a1ea5.jpg"
+                alt="Profile Image" class="w-42 h-42 mx-auto mb-4 shadow-md border-4 border-gray-200" />
+        </div>
+    </div>
+</div>
