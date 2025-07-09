12f08f8bc582ef6ab109918ce340fa4b2ce745e4
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-07-09 14:56:18 +0700

    chore(seeder): update seeder for tables

--- database/seeders/PersonalTrainersSeeder.php
+++ database/seeders/PersonalTrainersSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\PersonalTrainers;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PersonalTrainersSeeder extends Seeder
 {
